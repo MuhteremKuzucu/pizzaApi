aad2d18cbd12014d355886a32138246c09efc2ac
MuhteremKuzucu <kuzucumuhterem@gmail.com>
2025-03-18 10:35:33 +0100

    up

--- PizzaApi/src/controllers/token.js
+++ PizzaApi/src/controllers/token.js
@@ -8,17 +8,7 @@ const Token = require('../models/token');
 module.exports = {
     list: async (req, res) => {
         /* 
-            #swagger.tags = ['Tokens']
-            #swagger.summary = 'List Tokens'
-            #swagger.desription = `
-                You can sen query with endpoint for filter[], search[], sort[], page and limit.
-                <ul> Examples usage:
-                    <li>URL/?<b>filter[field1]=value1&filter[field2]=value2</b></li>
-                    <li>URL/?<b>search[field1]=value1&search[field2]=value2</b></li>
-                    <li>URL/?<b>sort[field1]=1&sort[field2]=-1</b></li>
-                    <li>URL/?<b>page=2&limit=1</b></li>
-                </ul>
-            `
+            #swagger.ignore = true
         */
 
         const result = await res.getModelList(Token);
@@ -32,8 +22,7 @@ module.exports = {
 
     create: async (req, res) => {
         /* 
-            #swagger.tags = ['Tokens']
-            #swagger.summary = 'Create Token'
+            #swagger.ignore = true
         */
 
         const result = await Token.create(req.body);
@@ -46,8 +35,7 @@ module.exports = {
 
     read: async (req, res) => {
         /* 
-            #swagger.tags = ['Tokens']
-            #swagger.summary = 'Get Single Token'
+            #swagger.ignore = true
         */
 
         const result = await Token.findOne({ _id: req.params.id });
@@ -60,8 +48,7 @@ module.exports = {
 
     update: async (req, res) => {
         /* 
-            #swagger.tags = ['Tokens']
-            #swagger.summary = 'Update Token'
+            #swagger.ignore = true
         */
 
         const result = await Token.updateOne({ _id: req.params.id }, req.body, { runValidators: true });
@@ -79,8 +66,7 @@ module.exports = {
 
     delete: async (req, res) => {
         /* 
-            #swagger.tags = ['Tokens']
-            #swagger.summary = 'Delete Token'
+            #swagger.ignore = true
         */
 
         const result = await Token.deleteOne({ _id: req.params.id });
